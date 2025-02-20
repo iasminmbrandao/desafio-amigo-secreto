05e9cad865cad7f7cdf0ee08a0e1c7c607746f3a
Iasmin Mendes <iasminmendes05@gmail.com>
2025-02-19 21:21:08 -0300

    Adiciona funcionalidade para adicionar amigos à lista e valida entrada de dados. Limpa campo de entrada após adicionar.

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,20 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = []; // Lista que vai armanezar os nomes dos amigos
+
+// Função para adicionar um amigo à lista
+function adicionarAmigo() {
+    let nomeAmigo = document.getElementById ('amigo').value.trim(); // Captura o valor digitado no campo e usa trim() para remover espaços extras
+   // Verifica se o campo está vazio
+    if (nomeAmigo == "") {
+        alert ('Por favor, insira um nome.');
+    } else {
+        amigos.push (nomeAmigo); // Adiciona o nome à lista de amigos
+    }
+
+    limparCampo(); // Limpa o campo de entrada após a adição
+    console.log (amigos); // Exibe a lista de amigos no console (para verificação)
+}
+
+// Função para limpar o campo de entrada
+function limparCampo() {
+    document.getElementById('amigo').value = '';
+}
