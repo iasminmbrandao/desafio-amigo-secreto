5164815635a677ad956c3c00eec7600807633726
Iasmin Mendes <iasminmendes05@gmail.com>
2025-02-22 14:47:20 -0300

    Implementa uma função para atualizar a lista de amigos e exibir na pagina (HTML).

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -12,9 +12,22 @@ function adicionarAmigo() {
 
     limparCampo(); // Limpa o campo de entrada após a adição
     console.log (amigos); // Exibe a lista de amigos no console (para verificação)
+    atualizarLista();
 }
 
 // Função para limpar o campo de entrada
 function limparCampo() {
     document.getElementById('amigo').value = '';
+}
+
+//Função para adicionar a lista na página
+function atualizarLista() {
+    const lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+    //Usando o loop for para percorrer a lista
+    for(let i=0; i < amigos.length; i++){  
+        const li = document.createElement('li');  // Criar o elemento <li>
+        li.textContent = amigos[i];   // Definir o texto do <li> como o nome do amigo
+        lista.appendChild(li);  // Adicionar o <li> à lista (ul)
+    }
 }
